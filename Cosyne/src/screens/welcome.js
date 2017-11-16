52d45c71bee3042fea0617dc401a2b33723eb107
jamesguan <jguan3@umbc.edu>
2017-11-16 13:29:50 -0500

    James: Added templating grid design for Welcome screen

--- Cosyne/src/screens/welcome.js
+++ Cosyne/src/screens/welcome.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-const appStyles = require('../components/styles');
-
+import PropTypes from 'prop-types';
 import {
   StyleSheet,
   Text,
@@ -9,45 +8,85 @@ import {
   PixelRatio,
 } from 'react-native';
 
+import {Navigation} from 'react-native-navigation';
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { Button, ButtonGroup } from 'react-native-elements';
+/*
 import {
   MKTextField,
   MKColor,
   mdl,
 } from 'react-native-material-kit';
+ */
 
-const styles = Object.assign({}, appStyles, StyleSheet.create({
-  col: {
-    flex: 1,
-    flexDirection: 'column',
-    // alignItems: 'center', // this will prevent TFs from stretching horizontal
-    marginLeft: 7, marginRight: 7,
-    // backgroundColor: MKColor.Lime,
+const appStyles = require('../components/styles');
+
+const styles = StyleSheet.create({
+  container: {
+
   },
-  textfield: {
-    height: 28,  // have to do it on iOS
-    marginTop: 32,
-  },
-  textfieldWithFloatingLabel: {
-    height: 48,  // have to do it on iOS
-    marginTop: 10,
-  },
-}));
+  scrollView: {
+
+  }
+});
 
 class Welcome extends Component {
   constructor(props){
     super(props);
+    this._toggleNavBar = 'hidden';
+    let to = this._toggleNavBar;
+    this.props.navigator.toggleNavBar({
+      to,
+      animated: false,
+    });
+    this.state = {
+      selectedIndex: 2
+    }
+    this.updateIndex = this.updateIndex.bind(this)
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
   }
   componentDidMount(){
 
   }
+  onNavigatorEvent(event) {
+    if (event.id === 'contextualMenuDismissed') {
+      this._contextualMenu = false;
+    }
+  }
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+  login = () => {
+
+  }
   render(){
+    const buttons = ['Hello', 'World', 'Buttons']
+    const { selectedIndex } = this.state;
     return (
-      <ScrollView style={styles.scrollView}
-                  contentContainerStyle={styles.container}>
-        <View>
-          
-        </View>
-      </ScrollView>
+          <Grid>
+            <Row size={50}>
+            </Row>
+            <Row size={25}>
+              <Col>
+                <Button
+                  raised
+                  icon={{name: 'launch'}}
+                  backgroundColor={'#397af8'}
+                  borderRadius ={12}
+                  title='Login'
+                />
+                <Button
+                  raised
+                  backgroundColor={'#397af8'}
+                  borderRadius ={12}
+                  icon={{name: 'flight'}}
+                  title='Register'
+                />
+              </Col>
+            </Row>
+            <Row size={25}>
+            </Row>
+          </Grid>
     );
   }
 }
